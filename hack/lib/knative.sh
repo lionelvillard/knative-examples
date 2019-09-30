abda047df7ddfb4016d479a4c309e9754a59f409
Lionel Villard <villard@us.ibm.com>
2019-09-30 17:26:01 -0400

    add test against eventing nightly

--- hack/lib/knative.sh
+++ hack/lib/knative.sh
@@ -19,34 +19,38 @@ function knative::install() {
   local serving_version=$1
   local eventing_version=$2
 
-  if [[ $serving_version == "" ||  $eventing_version == "" ]]; then
-    u::fatal "usage: knative::install <serving-version> <eventing-version>"
+  local serving_base=https://github.com/knative/serving/releases/download/v${serving_version}
+  local eventing_base=https://github.com/knative/eventing/releases/download/v${eventing_version}
+
+  if [[ $serving_version == "" || $serving_version == "nightly" ]]; then
+    echo "install knative serving nightly"
+    serving_base=https://storage.googleapis.com/knative-nightly/serving/latest
+  fi
+
+
+  if [[ $eventing_version == "" || $eventing_version == "nightly" ]]; then
+    echo "install knative eventing nightly"
+    eventing_base=https://storage.googleapis.com/knative-nightly/eventing/latest
   fi
 
   u::header "installing knative CRDS"
 
   # there is a bug in kubectl .. must do it twice.
   set +e
-  kubectl apply --selector knative.dev/crd-install=true -f https://github.com/knative/serving/releases/download/v${serving_version}/serving.yaml
-  kubectl apply --selector knative.dev/crd-install=true -f https://github.com/knative/serving/releases/download/v${serving_version}/serving.yaml
-  kubectl apply --selector knative.dev/crd-install=true -f https://github.com/knative/eventing/releases/download/v${eventing_version}/release.yaml
+  kubectl apply --selector knative.dev/crd-install=true -f ${serving_base}/serving.yaml
+  kubectl apply --selector knative.dev/crd-install=true -f ${serving_base}/serving.yaml
+  kubectl apply --selector knative.dev/crd-install=true -f ${eventing_base}/release.yaml
   set -e
 
   sleep 2
 
   u::header "installing knative"
 
-  kubectl apply -f https://github.com/knative/serving/releases/download/v${serving_version}/serving.yaml
+  kubectl apply -f  ${serving_base}/serving.yaml
   k8s::wait_until_pods_running knative-serving
 
-  kubectl apply -f https://github.com/knative/eventing/releases/download/v${eventing_version}/release.yaml
+  kubectl apply -f ${eventing_base}/release.yaml
   k8s::wait_until_pods_running knative-eventing
-
-  # kubectl apply -f  https://github.com/knative/serving/releases/download/v${serving_version}/monitoring.yaml
-  # kubectl patch -n knative-monitoring statefulsets.apps prometheus-system -p '{"spec":{"replicas":1}}'
-  # kubectl patch -n knative-monitoring statefulsets.apps elasticsearch-logging -p '{"spec":{"replicas":1}}'
-  # kubectl delete -n knative-monitoring deployments.apps grafana
-  # k8s::wait_until_pods_running knative-monitoring
 
   return 0
 }
